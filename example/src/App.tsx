1982a3cb4354add4b62569d711f724d3187e1a89
duguyihou <duguyihou@gmail.com>
2024-03-17 11:33:25 +1100

    5 update example (#6)
    * chore: install react-native-video
    
    * chore: wip

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,21 @@
-import * as React from 'react';
-
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import Video from 'react-native-video';
 import NeoAirplay, {
   useNeoAudioSessionRoutes,
   useNeoAirplayConnectivity,
 } from 'react-native-neo-airplay';
 
 export default function App() {
+  const uri =
+    'https://rbmn-live.akamaized.net/hls/live/590964/BoRB-AT/master_928.m3u8';
   const routes = useNeoAudioSessionRoutes();
   console.log(`🐵 ------ routes`, routes);
   const isAirplayConnected = useNeoAirplayConnectivity();
   console.log(`🐵 ------  isAirplayConnected`, isAirplayConnected);
   return (
     <View style={styles.container}>
+      <Video source={{ uri }} style={styles.backgroundVideo} />
       <NeoAirplay
         style={styles.box}
         tintColor="red"
@@ -30,8 +33,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   box: {
+    position: 'absolute',
+    bottom: 200,
     width: 60,
     height: 60,
-    marginVertical: 20,
+  },
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
 });
